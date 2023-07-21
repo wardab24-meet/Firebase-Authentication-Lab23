60304dd06742484e9c4d61732c9be63c1859c9d0
wardab24-meet <139688935+wardab24-meet@users.noreply.github.com>
2023-07-22 02:00:26 +0300

    .

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {
+    "apiKey": "AIzaSyDIoigRxqoG3yF1XhSEfnxnne1v3Yk4NLM",
+    "authDomain": "ayoubtheg-bbc44.firebaseapp.com",
+    "projectId": "ayoubtheg-bbc44",
+    "storageBucket": "ayoubtheg-bbc44.appspot.com",
+    "messagingSenderId": "981860393869",
+    "appId": "1:981860393869:web:4e354995d6a8783ae193c4",
+    "measurementId": "G-WGCERY71N7",
+    "databaseURL": ""
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
@@ -19,6 +33,8 @@ def signup():
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+
+
 
 
 if __name__ == '__main__':
